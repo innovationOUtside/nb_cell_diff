cd1884c9dde0fe7da8d4bd590b1b9c55f8063fee
Tony Hirst <tony.hirst@gmail.com>
2020-11-02 18:33:16 +0000

    setup longdesc

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
 from setuptools import setup
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+from os import path
+
+def get_long_description():
+    with open(
+        path.join(path.dirname(path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
 
 setup(
     name="nbcelldiff",
     author="Tony Hirst",
     author_email="tony.hirst@open.ac.uk",
     description="Tools for finding the difference between two notebook code cells",
-    long_description=long_description,
+    long_description=get_long_description(),
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/innovationOUtside/nb_cell_diff",
     packages=['nbcelldiff'],
+    license='MIT License',
     package_data = {},
     include_package_data=False,
     install_requires=[],
